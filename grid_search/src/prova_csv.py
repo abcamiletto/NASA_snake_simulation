92b674bcc56e16b730d9c16a20d76d240959d077
danielemorra98 <morradaniele@ymail.com>
2019-11-24 22:50:42 -0800

    csv4

--- grid_search/src/prova_csv.py
+++ grid_search/src/prova_csv.py
@@ -8,7 +8,7 @@ from control_sn.msg import param
 
 x = 0
 y = 0
-i = 0
+i = 1
 a_p = 0
 ot_p = 0
 ox_p = 0
@@ -36,17 +36,15 @@ while not rospy.is_shutdown():
     pos = rospy.Subscriber ('/my_odom', Pose2D, Callback1)
     par = rospy.Subscriber ('/param', param, Callback2)
     i+=1
-    csvRow = ['Tentativo' + str(i)]
+    csvRows = [['Tentativo' + str(i)], ['Amplitude Pitch', a_p], ['Spatial frequency Pitch', ox_p], ['Temporal frequency Pitch', ot_p], ['Amplitude Yaw', a_y], ['Spatial frequency Yaw', ox_y], ['Temporal frequency Yaw', ot_y], ['x', x], ['y', y]]
     csvfile = "data.csv"
-    with open(csvfile, "a") as fp:
-        wr = csv.writer(fp, dialect='excel')
-        wr.writerow(csvRow)
-        wr.writerow(['Amplitude Pitch', a_p])
-        wr.writerow(['Spatial frequency Pitch', ox_p])
-        wr.writerow(['Temporal frequency Pitch', ot_p])
-        wr.writerow(['Amplitude Yaw', a_y])
-        wr.writerow(['Spatial frequency Yaw', ox_y])
-        wr.writerow(['Temporal frequency Yaw', ot_y])
-        wr.writerow(['x', x])
-        wr.writerow(['y', y])
-    rospy.sleep(5)
+    with open(csvfile, 'r') as readFile:
+        read = csv.reader(readFile, dialect='excel')
+        lines = list(read)
+        lines[(i-1)*9+1] = csvRows[1], lines[(i-1)*9+2] = csvRows[2], lines[(i-1)*9+3] = csvRows[3], lines[(i-1)*9+4] = csvRows[4], lines[(i-1)*9+5] = csvRows[5], lines[(i-1)*9+6] = csvRows[6], lines[(i-1)*9+7] = csvRows[7], lines[(i-1)*9+8] = csvRows[8], lines[(i-1)*9+9] = csvRows[9],
+    with open(csvfile, 'w') as writeFile:
+        wr = csv.writer(writeFile, dialect='excel')
+        wr.writerows = lines
+    readFile.close()
+    writeFile.close()
+    rospy.sleep(0.2)
